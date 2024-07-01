a135d1493affdcae995b26d447d3a91dd8b702cd
ThiagoBD <thiagobarrosdias@gmail.com>
2024-07-01 15:37:30 -0300

    project created

---
+++ src/test/java/com/thiagobarrosdias/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagobarrosdias.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
